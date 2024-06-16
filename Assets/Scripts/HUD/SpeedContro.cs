449fd6ddcbb0e7135ab38ba37c79cd8877dac808
Eduardo Andres Sosa Segovia <eduardososasegovia@gmail.com>
2024-06-16 02:23:16 -0300

    Mejora de UI / implementacion de sfx y musica general

--- Assets/Scripts/HUD/SpeedContro.cs
+++ Assets/Scripts/HUD/SpeedContro.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using Simplon;
 using TMPro;
+using UnityEngine.UI;
+using Unity.VisualScripting;
 /*este script es el encargado de mostrar la velocidad del vehiculo en la vista del HuD*/
 public class SpeedContro : MonoBehaviour
 {
     //referencia al text donde se mostrara la velocidad tiene que ser un textmeshpro-textUI
     [SerializeField] private TextMeshProUGUI SpeedViewer;
+    [SerializeField]private Slider speedSlider;
+    [SerializeField] private AudioSource speedAudioSource;
+    private void Start() {
+    }
 
     // Update is called once per frame
     void Update()
@@ -17,5 +23,10 @@ public class SpeedContro : MonoBehaviour
     private void mostrarSpeed() {
         //muestra la velocidad 
         SpeedViewer.text = string.Format("{0}", GameControler.instance.ObtnerSpeed());
+        speedSlider.value = GameControler.instance.ObtnerSpeed();
+        float valorNormalizado = speedSlider.value / 55f; // Normaliza el valor original a 0-1
+        float valorInterpolado = Mathf.Lerp(0, 2, valorNormalizado); // Usa Mathf.Lerp para interpolar en el nuevo rango
+        float valorFinal = valorInterpolado * 2; // Desnormaliza el valor al rango 0-2
+        speedAudioSource.pitch = valorFinal;
     }
 }
